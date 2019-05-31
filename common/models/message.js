eadcc3b6495ebf8e4bb157eefa85cd10ff6d3674
Arun Shinde <arunshinde@hotmail.com>
2019-05-31 00:44:25 -0400

    chnaged the greet method on Message model

--- common/models/message.js
+++ common/models/message.js
@@ -1,8 +1,8 @@
 module.exports = function(Message) {
-  Message.greet = function(msg, cb) {
+  Message.greet = function(msg, name, cb) {
     process.nextTick(function() {
       msg = msg || 'hello';
-      cb(null, 'Sender says ' + msg + ' to receiver');
+      cb(null, 'Sender says ' + msg + ' to ' + name + ' receiver');
     });
   };
 };
